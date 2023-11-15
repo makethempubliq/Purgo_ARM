30b09609ef5e3d311c897c587108dd6e2973edd2
makethempubliq <147483560+makethempubliq@users.noreply.github.com>
2023-11-15 14:22:03 +0900

    20231115 신원식 비번변경초기화

--- loginregister.py
+++ loginregister.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, PasswordField, EmailField
+from wtforms import StringField, TextAreaField, PasswordField, EmailField, SubmitField
 from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 class UserCreateForm(FlaskForm):
@@ -13,3 +13,13 @@ class UserCreateForm(FlaskForm):
 class UserLoginForm(FlaskForm):
     email = EmailField('이메일', validators=[DataRequired(), Email()])
     password = PasswordField('비밀번호', validators=[DataRequired()])
+
+class ResetPasswordForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Reset Password')
+
+class ChangePasswordForm(FlaskForm):
+    email = EmailField('이메일', validators=[DataRequired(), Email()])
+    password = PasswordField('기존 비밀번호', validators=[DataRequired()])
+    newpassword = PasswordField('새로운 비밀번호', validators=[DataRequired()])
+    submit = SubmitField('Change Password')
